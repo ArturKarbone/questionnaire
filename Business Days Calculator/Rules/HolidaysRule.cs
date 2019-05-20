0db6264d7b39a05953c23738c78c831d9c9e4d3f
Arturs Karbone <arturs.karbone@if.lv>
2019-05-20 21:42:20 +0300

    introduce holidays rule

---
+++ Business Days Calculator/Rules/HolidaysRule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BusinessDays.Rules
+{
+    public class HolidaysRule : IRule
+    {
+        public bool IsBusinessDay(DateTime date)
+        {
+            return !IsChristmas() && !IsNewYear();
+
+            bool IsChristmas() => ((date.Day == 24 || date.Day == 25) && date.Month == 12);
+            bool IsNewYear() => ((date.Day == 32 && date.Month == 12) || (date.Day == 1 && date.Month == 1));
+        }
+    }
+}
